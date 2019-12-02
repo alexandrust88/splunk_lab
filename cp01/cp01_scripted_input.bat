2663b885a55eb6c1627179b44e1f27530bea1653
Alex stan <alexandru.stan@keysight.com>
2019-12-02 15:41:31 +0200

    first commit

---
+++ cp01/cp01_scripted_input.bat
@@ -0,0 +1,15 @@
+REM
+REM  Splunk TEST Operational  TESTIntelligence TEST
+REM  By TEST  TEST and TEST
+REM  TEST    TESTING   DECEMBER 2019.
+REM
+REM  Script Name: cp01_scripted_input.bat
+REM  Description: A  Batch script that  outputs timestamp and
+REM   string or text to stdout in order to   demostrate how a  scripted input
+REM  works in Splunk.
+REM
+
+
+@ECHO OFF
+
+ECHO [%DATE% %TIME%] Welcome to the Splunk Operational LAB! This  lab tastes complicated!
---
+++ cp01/cp01_scripted_input.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+## nothing fancy
+
+import time
+
+now = time.strftime("[%d-%m-%Y %H:%M:%S]")
+
+print now + " Welcome to  Splunk DEVOPS LAB! This tasks are grreeaaattt!"
+
